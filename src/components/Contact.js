cde354fe9db055012bbf81a67eac4c1750a169c6
Daniel <danielo.visage@gmail.com>
2020-11-20 12:45:19 +0100

    quest 2.1 done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,13 +5,20 @@ const avatar = "https://randomuser.me/api/portraits/men/94.jpg";
 const name = "Wyatt Harris";
 const online = true;
 
+let status = null;
+
 const Contact = () => {
+  online ? (status = "Online") : (status = "Offline");
+
   return (
-    <div className="contact">
-      <img className="contact avatar" src={avatar} alt="profile-pic" />
+    <div className="Contact">
+      <img className="avatar" src={avatar} alt="profile-pic" />
       <div>
-        <h2 className="contact name">{name}</h2>
-        <p className="status status-text">{online ? "Online" : "Offline"}</p>
+        <h2 className="name">{name}</h2>
+        <div className="status">
+          <div className={online ? "status-online" : "status-offline"}></div>
+          <p className="status-text">{status}</p>
+        </div>
       </div>
     </div>
   );
